a615dc26fa0e46e4d1580311021d4de269f3ce0a
Zhou Lang <zhoulang117@gmail.com>
2015-08-21 10:37:15 -0600

    03/19/2015
    1. This code is based code written on 03/05/2015, some bugs are fixed.
    
    2. Simulation of a_{0.05}=4.18 is done for verification purpose

--- achive.R
+++ achive.R
@@ -1,20 +1,23 @@
 ## +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
 ## Reproduce results of Eubank (1997)
 
-## Last Updated: 03/05/2015
+## Last Updated: 03/19/2015
 
-## This is the initial code I wrote, it has some bugs.
+## This code is based code written on 03/05/2015, some bugs are fixed.
+## Simulation of a_{0.05}=4.18 is done for verification purpose
 ## +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
 eubank <- function(K, n, p, T){
-    rej <- 0                           #initial value of number of rejections
-    rej_classical <- 0                 #initial value of rejects of classical test
+    rej_new<- 0                        #initial of rejects of the new data-driven method
+    rej_newq <- 0                      #initial of rejects of new method using q
+    rej_classical <- 0                 #initial of rejects of classical test
+    rej_transform <- 0                 #initial of rejects of transformed classcial test
     Q <- rep(NA, T)                    #vector storage of all qhat's
     p0 <- rep(1/K, K)                  #value of H0, using capital K
     J <- K-1                           #J is always K-1
     q <- seq(0, K-1, by=1)             #q is from 0 to 9
     M <- rep(0, length(q))             #initial M with M(q=0)=0
     coef1 <- (n+1)/(n-1)               #coeficient 1 of M
-    coef2 <- 2/(n-1)                   #coeficient 2 of M
+    coef2 <- 4.18/(n-1)                #coeficient 2 of M, 4.18 is for a_{0.05}
     X <- matrix(NA, J, K)              #matrix of Fourier coefficients
     for (j in 1:J){
         for (k in 1:K){
@@ -24,33 +27,40 @@ eubank <- function(K, n, p, T){
     for (t in 1:T){
         data <- rmultinom(1, n, prob=p)         #generate data using real prob
         phat <- as.vector(data/n)               #\hat{p} is calculated by simulated data
-        ## classical chi-sqaure test
-        Xsquare_classical <- n*sum(((phat-p0)^2)/p0)
+        fhat <- as.matrix((phat - p0)/sqrt(p0)) #matrix of \hat{f(k)}
+        b <- X %*% fhat                         #b1-b9 are generated                
+        ## classical chi-sqaure test, alpha=0.05
+        Xsquare_classical <- n*sum(fhat^2)
         if (Xsquare_classical > qchisq(0.95, K-1)){
             rej_classical <- rej_classical + 1
         }else{
             rej_classical <- rej_classical
         }
-        ## paper's chi-square test
-        fhat <- as.matrix((phat - p0)/sqrt(p0)) #matrix of \hat{f(k)}
-        b <- X %*% fhat                         #b1-b9 are generated
+        ## transformed chi-square test, alpha=0.05
+        Xsquare_transform <- n*sum(b^2)
+        if (Xsquare_transform > qchisq(0.95, K-1)){
+            rej_transform <- rej_transform + 1
+        }else{
+            rej_transform <- rej_transform
+        }
+        ## paper's new chi-square test, alpha=0.05
         v <- (X^2) %*% (phat/p0)                #v_jj for M(q)
-        for (q in 1:J){
-            M[q+1] <- coef1*sum(b[1:q, 1]) - coef2*sum(v[1:q, 1])
+        for (qq in 1:J){
+            M[qq+1] <- coef1*sum((b[1:qq, 1])^2) - coef2*sum(v[1:qq, 1])
         }
         qhat <- which.max(M) - 1                #q is from 0 to 9
         Q[t] <- qhat
         if (qhat != 0){
-            Xsquare <- n*sum((b^2)[1:qhat, 1])      #test statistic
-            critical <- qchisq(0.95, qhat)       #critial chi-sq value for alpha=0.05
-            ## critical <- 4.18     #paper's critical value, which doesn't work well
-            if (Xsquare > critical){
-                rej <- rej + 1
+            Xsquare_new <- n*sum((b[1:qhat, 1])^2)      #test statistic
+            rej_newq <- rej_newq + 1
+            if (Xsquare_new > qchisq(0.95, qhat)){
+                rej_new <- rej_new+ 1
             }else{
-                rej <- rej
+                rej_new <- rej_new
             }
         }else{
-            rej <- rej
+            rej_new<- rej_new
+            rej_newq <- rej_newq
         }
         ## show progress
         if (t==1 | t==T){
@@ -63,84 +73,109 @@ eubank <- function(K, n, p, T){
             flush.console()
         }
     }
-    reject <- rej/T; reject_classical <- rej_classical/T
-    list(reject = reject, Q = Q, reject_classical = reject_classical)
+    reject_new <- rej_new/T
+    reject_newq <- rej_newq/T
+    reject_classical <- rej_classical/T
+    reject_transform <- rej_transform/T
+    list(reject_new = reject_new, reject_newq = reject_newq, Q = Q,
+         reject_classical = reject_classical, reject_transform = reject_transform)
 }
+
+
+
+## simulation of a_{0.05}=4.18 for verification
+K <- 10
+a <- 4.18
+T <- 5000
+count <- 0
+for (t in 1:T){
+    test <- rep(NA, K-1)
+    for (k in 1:(K-1)){
+        test[k] <- (1/k)*rchisq(1, df=k)
+    }
+    if (max(test) >= a){
+        count <- count + 1
+    }else{
+        count <- count
+    }
+}
+alpha <- count/T
+print(alpha)
+
 
 
 
 ## The following code simulates alpha=0.05
-## individual case with T=600000
+## individual case with T=10,000
 K <- 10                                 #capital K
-n <- 150                                #total number draws
-p <- rep(1/K, K)                        #set real prob for simulation alpha
-T <- 600000
-sim <- eubank(K, n, p, T)
-sink("individual.Rout",  append=FALSE)
-## print simlulated type I error
-print(sim$reject); print(sim$reject_classical)
-## print tables for all qhat's
-print(table(sim$Q)); print(round(table(sim$Q)/T, 3))
-sink()
-
-## plot K vs. Alpha and n vs. Alpha
-## K vs. Alpha, when n=150
-n <- 150                                #total number draws
-T <- 600000
-testK <- seq(3, 50, by=1)
-myreject1 <- rep(NA, length(testK))
-for (i in 1:length(testK)){
-    K <- testK[i]; p <- rep(1/K, K)
-    sim <- eubank(K, n, p, T)
-    myreject1[i] <- sim$reject    
-}
-## n vs. Alpha when K=10
-K <- 10                                 #capital K
-p <- rep(1/K, K)                        #set real prob for simulation alpha
-T <- 600000
-testn <- seq(50, 500, by=5)
-myreject2 <- rep(NA, length(testn))
-for (i in 1:length(testn)){
-    n <- testn[i]
-    sim <- eubank(K, n, p, T)
-    myreject2[i] <- sim$reject
-}
-## Plot
-pdf("alpha.pdf", height=5, width=10)
-par(mfrow=c(1, 2))
-plot(testK, myreject1, type="o", main="K vs. Alpha, when n=150", xlab="K", ylab="Alpha")
-plot(testn, myreject2, type="o", main="n vs. Alpha, when K=10", xlab="n", ylab="Alpha")
-dev.off()
-
-## The following code simulates power when alpha=0.05 with n=75, 150 and K=10
-K <- 10                                 #capital K
-T <- 600000
+T <- 10000                              #Paper used 10,000 iterations
 beta <- seq(0, 0.07, by=0.01)
-testp <- matrix(NA, length(beta), K)
+allp <- matrix(NA, length(beta), K)
 for (i in 1:length(beta)){
     for (k in 1:K){
-        testp[i, k] <- 0.1+beta[i]*(k-5.5)/10  #set real prob for simulation alpha
+        allp[i, k] <- 0.1+beta[i]*(k-5.5)/10  #set real prob for simulation alpha
     }
 }
-## beta vs. power.
-n <- 75                                #total number draws
-myreject3 <- rep(NA, length(beta))
-for (i in 1:length(beta)){
-    p <- as.vector(testp[i,])
+
+## Simulate the power of original and transformed classical chi-square
+## test using both my code and R build-in function
+power_myclass <- rep(NA, length(beta))
+power_R <- rep(NA, length(beta))
+power_mytransform <- rep(NA, length(beta))
+for (kk in 1: length(beta)){
+    p <- allp[kk,]
+    n <- 75
+    ## n <- 150
+    ## my code
     sim <- eubank(K, n, p, T)
-    myreject3[i] <- sim$reject
+    power_myclass[kk] <- sim$reject_classical
+    power_mytransform[kk] <- sim$reject_transform
+    ## R build-in function "chisq.test()"
+    rej_R <- 0
+    for (i in 1:T){
+        data <- as.vector(rmultinom(1, n, prob=p))   #generate data using real prob
+        Rtest <- chisq.test(data, p=rep(1/K, K))
+        if (Rtest$p.value < 0.05){
+            rej_R <- rej_R + 1
+        }else{
+            rej_R <- rej_R
+        }
+    }
+    power_R[kk] <- rej_R/T
 }
-n <- 150                                #total number draws
-myreject4 <- rep(NA, length(beta))
-for (i in 1:length(beta)){
-    p <- as.vector(testp[i,])
+
+pdf("1_classical.pdf", height=5, width=7.5)
+par(mfrow=c(1, 1))
+plot(beta, power_myclass, ylim=c(0,1), ylab="Power", col=1, lty=1, type="b",
+     main="Comparison of Classical Chi-Square, n=")
+points(beta, power_R, col=2, lty=2, type="b")
+points(beta, power_mytransform, col=3, lty=3, type="b")
+legend("topright", c("My Classical", "R Function", "My Transform"), col=c(1,2,3),
+       lty=1:3)
+dev.off()
+
+
+## Simulate the power of the new data-driven method
+power_new <- rep(NA, length(beta))
+power_newq <- rep(NA, length(beta))
+power_class <- rep(NA, length(beta))
+for (kk in 1:length(beta)){
+    p <- allp[kk,]
+    n <- 75
+    ## n <- 150
     sim <- eubank(K, n, p, T)
-    myreject4[i] <- sim$reject
+    power_new[kk] <- sim$reject_new
+    power_newq[kk] <- sim$reject_newq
+    power_class[kk] <- sim$reject_classical
 }
-## plot
-pdf("power.pdf", height=5, width=10)
-par(mfrow=c(1, 2))
-plot(beta, myreject3, type="o", main="Beta vs. Power, n=75", xlab="Beta", ylab="Power")
-plot(beta, myreject4, type="o", main="Beta vs. Power, n=150", xlab="Beta", ylab="Power")
+
+pdf("3_new.pdf", height=5, width=7.5)
+par(mfrow=c(1, 1))
+plot(beta, power_new, ylim=c(0,1), ylab="Power", col=1, lty=1, type="b",
+     main="Comparison of New and Classical Chi-Square, n=, a_(0.05)=4.18")
+points(beta, power_newq, col=2, lty=2, type="b")
+points(beta, power_class, col=3, lty=3, type="b")
+legend("topright", c("New Method", "New Method using q", "Classical"), col=c(1,2,3),
+       lty=1:3)
 dev.off()
 ############################# END ###########################################
